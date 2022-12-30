2d12e1e971988bcc5083d9f54a84172f2fc6b25d
Aleksey Khaliulin <akhaliulin@gmail.com>
2022-12-30 16:42:33 +0700

    initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,100 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "nginx1"
+  zone = "ru-central1-a"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8jvcoeij6u9se84dt5"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet1-zone-a.id
+    nat = true
+  }
+  
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "nginx2"
+  zone = "ru-central1-b"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8jvcoeij6u9se84dt5"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet1-zone-b.id
+    nat = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+
+  scheduling_policy {
+    preemptible = true
+  }
+}
+resource "yandex_vpc_network" "network-1" {
+  name = "network-1"
+}
+
+resource "yandex_vpc_subnet" "subnet1-zone-a" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = "${yandex_vpc_network.network-1.id}"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+resource "yandex_vpc_subnet" "subnet1-zone-b" {
+  name           = "subnet2"
+  zone           = "ru-central1-b"
+  network_id     = "${yandex_vpc_network.network-1.id}"
+  v4_cidr_blocks = ["192.168.11.0/24"]
+}
+
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
